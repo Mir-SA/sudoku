51e0dd56ff6b3acbd4d4954383729d8d7283f731
Mir <65391345+Mir-SA@users.noreply.github.com>
2024-01-07 19:57:06 +0530

    required features

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,109 @@
+const puzzleStrings = require("./puzzle-strings");
+
 class SudokuSolver {
+  constructor(puzzleString) {
+    this.grid = this.validate(puzzleString);
+  }
 
   validate(puzzleString) {
+    let grid = [];
+
+    for (let i = 0; i < 9; i++) {
+      grid[i] = [];
+      for (let j = 0; j < 9; j++) {
+        let index = i * 9 + j;
+        grid[i][j] =
+          puzzleString[index] === "." ? 0 : parseInt(puzzleString[index], 10);
+      }
+    }
+    return grid;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(grid, row, col, value) {
+    // let grid = this.validate(puzzleString);
 
+    // console.log('Value: ', grid[row][col-1])
+    if (grid[row][col] === parseInt(value)) {
+      return true;
+    } else {
+      for (let i = 0; i < 9; i++) {
+        // console.log(grid[row][i]);
+        if (grid[row][i] === parseInt(value)) return false;
+      }
+      return true;
+    }
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(grid, row, col, value) {
+    // let grid = this.validate(puzzleString);
 
+    if (grid[row][col - 1] === parseInt(value)) {
+      return true;
+    } else {
+      for (let i = 0; i < 9; i++) {
+        // console.log(grid[i][col-1]);
+        if (grid[i][col] === parseInt(value)) return false;
+        // if (grid[row][i] === value || grid[col][i] === value) return false;
+      }
+      return true;
+    }
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
+  checkRegionPlacement(grid, row, col, value) {
+    // let grid = this.validate(puzzleString);
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
+    // console.log(startRow, startCol, value);
+    // console.log("Value: ", row, col);
+    if (grid[row][col] === parseInt(value)) return true;
+    else {
+      for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+          if (grid[i][j] === parseInt(value)) return false;
+        }
+      }
+      return true;
+    }
   }
 
-  solve(puzzleString) {
-    
+  solve(grid) {
+    if (this.solveSudoku(grid)) {
+      return {
+        solved: true,
+        grid: grid,
+      };
+    } else {
+      return {
+        solved: false,
+        grid: null,
+      };
+    }
+  }
+
+  solveSudoku(grid) {
+    // let grid = this.validate(puzzleString);
+    // console.log(grid);
+
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (parseInt(grid[row][col]) === 0) {
+          for (let num = 1; num <= 9; num++) {
+            if (
+              this.checkRowPlacement(grid, row, col, num) &&
+              this.checkColPlacement(grid, row, col, num) &&
+              this.checkRegionPlacement(grid, row, col, num)
+            ) {
+              grid[row][col] = num;
+              if (this.solveSudoku(grid)) return true;
+              grid[row][col] = 0;
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,83 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+  let string = "";
+  let regex = /^[1-9.]+$/;
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzle } = req.body;
+    const { coordinate } = req.body;
+    const { value } = req.body;
+    const rowObj = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6, H: 7, I: 8 };
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    if (
+      value == null ||
+      value == "" ||
+      coordinate == null ||
+      coordinate == "" ||
+      puzzle == null ||
+      puzzle == ""
+    ) {
+      res.send({ error: "Required field(s) missing" });
+    } else if (puzzle.length !== 81) {
+      res.send({ error: "Expected puzzle to be 81 characters long" });
+    } else if (!regex.test(puzzle)) {
+      res.send({ error: "Invalid characters in puzzle" });
+    } else if (
+      coordinate.slice(1) > 9 ||
+      coordinate.slice(1) < 1 ||
+      coordinate.slice(0, 1) > "I"
+    ) {
+      res.send({ error: "Invalid coordinate" });
+    } else if (value > 9 || value < 1 || isNaN(parseInt(value))) {
+      res.send({ error: "Invalid value" });
+    } else {
+      let solver = new SudokuSolver(puzzle);
+      const row = coordinate.slice(0, 1);
+      const col = coordinate.slice(1) - 1;
+      // console.log(col);
+      const conflict = [];
+      let valid = true;
 
-    });
+      // console.log(solver.checkRowPlacement(puzzle, rowObj[row], col, value));
+      if (!solver.checkRowPlacement(solver.grid, rowObj[row], col, value)) {
+        // console.log(row);
+        valid = false;
+        conflict.push("row");
+      }
+      if (!solver.checkColPlacement(solver.grid, rowObj[row], col, value)) {
+        valid = false;
+        conflict.push("column");
+      }
+      if (!solver.checkRegionPlacement(solver.grid, rowObj[row], col, value)) {
+        valid = false;
+        conflict.push("region");
+      }
+      // console.log(valid === false ? { valid, conflict } : { valid });
+      valid === false ? res.send({ valid, conflict }) : res.send({ valid });
+    }
+  });
+
+  app.route("/api/solve").post((req, res) => {
+    string = req.body.puzzle;
+    let solver = new SudokuSolver(string);
+
+    if (string == null || string == "") {
+      res.send({ error: "Required field missing" });
+    } else if (string.length !== 81) {
+      res.send({ error: "Expected puzzle to be 81 characters long" });
+    } else if (!regex.test(string)) {
+      res.send({ error: "Invalid characters in puzzle" });
+    } else {
+      let sol = solver.solve(solver.grid);
+      // console.log("Grid: ", sol);
+      if (sol.solved === false) {
+        res.send({ error: "Puzzle cannot be solved" });
+      } else {
+        res.send({ solution: sol.grid.flat().join("") });
+      }
+    }
+  });
 };
